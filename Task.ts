b9e11832c974646cfd1b3ddbc20d9b1042daf137
JUlio Camus <47275128+Julcamus@users.noreply.github.com>
2020-01-29 23:38:45 +0100

    Add files via upload
    Last version of task code

--- Task.ts
+++ Task.ts
@@ -1,11 +1,58 @@
-import{DATE} from "date.ts" ; 
-import { Priority} from "priority.ts" ; 
-class Task {
-        content : string ;
-        date : DATE ;
-        priority : Priority ; 
+import  "date.ts";
+import "priority.ts";
+export class task {
+    content : string;
+    Date : MaDate;
+    priority : Priority;
+    checked : boolean = false;
 
+    getcontent (): string {
+        return this.content;
+    }
+    setcontent (content : string){
+        this.content = content ;
+    }
+    getDate ():MaDate{
+        return this.Date;
+    }
+    setDate (Date : MaDate){
+        this.Date= Date;
+    }
+    getPriority (): Priority{
+        return this.priority;
+    }
+    setPriority (priority:Priority){
+        this.priority=priority;
+    }
+    getchecked():boolean{
+        return this.checked;
+    }
+    setchecked (checked : boolean){
+        this.checked=checked;
+    }
+    add(content : string, Date : MaDate, priority : Priority) {
+        let id : number ;
+        return id ;
+    }
+    update(content? : string, Date? : MaDate, priority? : Priority, checked? : boolean){
+        this.content=content;
+        this.Date= Date;
+        this.priority=priority;
+        this.checked=checked;
 
-
-
+    }
+    close():void
+    {
+        this.checked = true;
+        
+    }
+    toString() : string{
+        let etat : string 
+        if (this.checked==true){
+            etat = "Effectu¨¦"
+        }else{
+            etat = "Non effectu¨¦"
+        }
+        return "Contenu : Apprendre Typescript \n Date : "+this.Date+"\n Priority"+this.priority+"\n Etat :"+ etat ;
+    }
 }
